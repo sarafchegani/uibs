c8fd57447b25d05179967d2a7fb8dce49eb7bbb5
sarafchegani <ghs.chegani@gmail.com>
2019-04-29 10:38:13 +0430

    a

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ZEOH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Last Command Time";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ZEEI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication5
+{
+   
+    public partial class Form1 : Form
+    {
+        public int fna = 0;
+        public int fn = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            TelnetConnection tc = new TelnetConnection("10.49.38.158", 23);
+            // FileInfo out_file = new FileInfo(@"C:\nokia_out\new.txt");
+            //login with user "root",password "rootpassword", using a timeout of 100ms, 
+            //and show server output
+            string s = tc.Login("TAHERE", "TAHERE123", 10);
+            // Console.Write(s);
+
+            // server output should end with "{1}quot; or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            //  prompt = s.Substring(prompt.Length - 1, 1);
+            // if (prompt != ":")
+            //     throw new Exception("Connection failed");
+
+            // prompt = "";
+
+            // while connected
+            // execute MML command
+            string sdate = DateTime.Now.TimeOfDay.ToString().Substring(0, 8).Replace(":", "-");
+            tc.WriteLine("EOH:," + sdate + ";");
+            fna = fna + 1;
+            if (fna == 10)
+                fna = 1;
+            // write to text file
+            FileStream fs = new FileStream(@"C:\nokia_out\ZEOH000" + fna.ToString() + ".txt", FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(tc.Read());
+            sw.Close();
+            //Console.WriteLine("***COMMITED");
+           textBox1.Text = DateTime.Now.ToString().Substring(0, 10);
+           textBox2.Text = DateTime.Now.TimeOfDay.ToString().Substring(0, 8).Replace(":","-"); 
+
+            //     while (tc.IsConnected && prompt.Trim() != "exit")
+            //     {
+            //         // display server output
+            //        Console.Write(tc.Read());
+
+            //         // send client input to server
+            //        prompt = Console.ReadLine();
+            //        tc.WriteLine(prompt);
+
+            //        // display server output
+            //         Console.Write(tc.Read());
+            //     }
+
+            //    Console.WriteLine("***DISCONNECTED");
+            // Console.ReadLine();
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            TelnetConnection tc1 = new TelnetConnection("10.49.38.158", 23);
+            // FileInfo out_file = new FileInfo(@"C:\nokia_out\new.txt");
+            //login with user "root",password "rootpassword", using a timeout of 100ms, 
+            //and show server output
+            string s = tc1.Login("TAHERE", "TAHERE123", 10);
+            // Console.Write(s);
+
+            // server output should end with "{1}quot; or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            //  prompt = s.Substring(prompt.Length - 1, 1);
+            // if (prompt != ":")
+            //     throw new Exception("Connection failed");
+
+            // prompt = "";
+
+            // while connected
+            // execute MML command
+
+            tc1.WriteLine("ZEEI;");
+            fn = fn + 1;
+            if (fn == 10)
+                fn = 1;
+            // write to text file
+            FileStream fs1 = new FileStream(@"C:\nokia_out\ZEEI000"+fn.ToString()+".txt", FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter sw1 = new StreamWriter(fs1);
+            sw1.Write(tc1.Read());
+            sw1.Close();
+            //Console.WriteLine("***COMMITED");
+            textBox1.Text = DateTime.Now.ToString().Substring(0, 10);
+            textBox2.Text = DateTime.Now.TimeOfDay.ToString().Substring(0, 8);
+           
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            TelnetConnection tc = new TelnetConnection("10.49.38.158", 23);
+            // FileInfo out_file = new FileInfo(@"C:\nokia_out\new.txt");
+            //login with user "root",password "rootpassword", using a timeout of 100ms, 
+            //and show server output
+            string s = tc.Login("TAHERE", "TAHERE123", 10);
+            // Console.Write(s);
+
+            // server output should end with "{1}quot; or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            //  prompt = s.Substring(prompt.Length - 1, 1);
+            // if (prompt != ":")
+            //     throw new Exception("Connection failed");
+
+            // prompt = "";
+
+            // while connected
+            // execute MML command
+            string sdate = DateTime.Now.TimeOfDay.ToString().Substring(0, 8).Replace(":", "-");
+            tc.WriteLine("EOH:," + sdate + ";");
+            fna = fna + 1;
+            if (fna == 10)
+                fna = 1;
+            // write to text file
+            FileStream fs = new FileStream(@"C:\nokia_out\ZEOH000" + fna.ToString() + ".txt", FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(tc.Read());
+            sw.Close();
+            //Console.WriteLine("***COMMITED");
+            textBox1.Text = DateTime.Now.ToString().Substring(0, 10);
+            textBox2.Text = DateTime.Now.TimeOfDay.ToString().Substring(0, 8).Replace(":", "-");
+
+            //     while (tc.IsConnected && prompt.Trim() != "exit")
+            //     {
+            //         // display server output
+            //        Console.Write(tc.Read());
+
+            //         // send client input to server
+            //        prompt = Console.ReadLine();
+            //        tc.WriteLine(prompt);
+
+            //        // display server output
+            //         Console.Write(tc.Read());
+            //     }
+
+            //    Console.WriteLine("***DISCONNECTED");
+            // Console.ReadLine()
+        }
+    }
+}
